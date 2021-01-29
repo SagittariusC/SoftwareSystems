772294819f9c16a21684768bb9eca6bf6e408bc4
Owen <owentern@gmail.com>
2021-01-29 11:45:15 -0800

    fixed rotation crash bug
    crashed after second photo because not enough memory, allocated more

--- app/src/main/java/com/example/gallery/MainActivity.java
+++ app/src/main/java/com/example/gallery/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.gallery;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
@@ -13,11 +14,14 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
 import android.media.ExifInterface;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
@@ -30,8 +34,10 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
@@ -47,6 +53,7 @@ public class MainActivity extends AppCompatActivity {
     TextView date_time;
     EditText caption;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -60,14 +67,30 @@ public class MainActivity extends AppCompatActivity {
         date_time = findViewById(R.id.timestamp);
         File files[] = getExternalFilesDir(Environment.DIRECTORY_PICTURES).listFiles();
 
+
+
         if (files.length > 0) {
             img_counter = files.length - 1;
-            selectedImage.setImageURI(Uri.fromFile(files[img_counter]));
-            String lastModDate = new SimpleDateFormat("dd/MM/yyyy_HH:mm:ss").format(new Date(files[img_counter].lastModified()));
-            date_time.setText(lastModDate);
-            caption.setText(updateCaption(files[img_counter].toString()));        }
+            updateCaption(files[img_counter]);
+        }
+
+        String[] searchResults = new String[3];
+        if (savedInstanceState == null) {
+            Bundle extras = getIntent().getExtras();
+            if(extras == null) {
+                searchResults = null;
+            } else {
+                searchResults[0] = extras.getString("STARTTIMESTAMP");
+                searchResults[1] = extras.getString("ENDTIMESTAMP");
+                searchResults[2] = extras.getString("CAPTION");
+                searchUpdate(files, searchResults);
+            }
+        } else {
+            searchResults[0] = (String) savedInstanceState.getSerializable("CAPTION");
+        }
 
         camera.setOnClickListener (new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.Q)
             @Override
             public void onClick (View v) {
                 askCameraPermissions();
@@ -92,6 +115,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
                 if(actionId== EditorInfo.IME_ACTION_DONE) {
+                    img_counter = 0;
                     String cap = caption.getText().toString();
                     File files[] = (getExternalFilesDir(Environment.DIRECTORY_PICTURES).listFiles());
                     updatePhoto(files[img_counter].getPath(), cap);
@@ -107,10 +131,8 @@ public class MainActivity extends AppCompatActivity {
         File files[] = getExternalFilesDir(Environment.DIRECTORY_PICTURES).listFiles();
         if (files.length > 1 && img_counter > 0) {
             img_counter--;
-            selectedImage.setImageURI(Uri.fromFile(files[img_counter]));
-            String lastModDate = new SimpleDateFormat("dd/MM/yyyy_HH:mm:ss").format(new Date(files[img_counter].lastModified()));
-            date_time.setText(lastModDate);
-            caption.setText(updateCaption(files[img_counter].toString()));
+            updateCaption(files[img_counter]);
+
         } else if (img_counter == 0) {
             Toast.makeText(this, "No more pictures!", Toast.LENGTH_SHORT).show();
         }
@@ -120,10 +142,7 @@ public class MainActivity extends AppCompatActivity {
         File files[] = getExternalFilesDir(Environment.DIRECTORY_PICTURES).listFiles();
         if (files.length > 1 && img_counter < files.length - 1) {
             img_counter++;
-            selectedImage.setImageURI(Uri.fromFile(files[img_counter]));
-            String lastModDate = new SimpleDateFormat("dd/MM/yyyy_HH:mm:ss").format(new Date(files[img_counter].lastModified()));
-            date_time.setText(lastModDate);
-            caption.setText(updateCaption(files[img_counter].toString()));
+            updateCaption(files[img_counter]);
         } else if (img_counter == files.length - 1) {
             Toast.makeText(this, "No more pictures!", Toast.LENGTH_SHORT).show();
         }
@@ -134,6 +153,7 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.Q)
     private void askCameraPermissions() {
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.CAMERA}, CAMERA_PERM_CODE);
@@ -142,6 +162,7 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.Q)
     @Override
     public void onRequestPermissionsResult (int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         if (requestCode == CAMERA_PERM_CODE) {
@@ -153,21 +174,38 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    @Override
-    protected void onActivityResult (int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == REQUEST_TAKE_PHOTO) {
-            if (resultCode == Activity.RESULT_OK) {
-                File f = new File(currentPhotoPath);
-                selectedImage.setImageURI(Uri.fromFile(f));
-                String lastModDate = new SimpleDateFormat("dd/MM/yyyy_HH:mm:ss").format(new Date(f.lastModified()));
-                date_time.setText(lastModDate);
-                caption.setText(updateCaption(currentPhotoPath));            }
+    private File rotateImage(File oldImage, int degree) throws IOException {
+        String imageFileName = oldImage.getName();
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+
+        Matrix matrix = new Matrix();
+        matrix.postRotate(degree);
+        Bitmap loadedImg = BitmapFactory.decodeFile(oldImage.getPath());
+
+        Bitmap rotatedImg = Bitmap.createBitmap(loadedImg, 0, 0, loadedImg.getWidth(), loadedImg.getHeight(), matrix, true);
+        loadedImg.recycle();
+        oldImage.delete();
+
+        File newImage = new File(storageDir, imageFileName);
+        if (!newImage.exists()) {
+            Log.d("path", newImage.toString());
+            FileOutputStream fos = null;
+            try {
+                fos = new FileOutputStream(newImage);
+                rotatedImg.compress(Bitmap.CompressFormat.JPEG, 100, fos);
+                fos.flush();
+                fos.close();
+            } catch (java.io.IOException e) {
+                e.printStackTrace();
+            }
         }
+        return newImage;
+
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.Q)
     private File createImageFile() throws IOException {
-        String timeStamp = new SimpleDateFormat("yyyyMMdd_HH:mm:ss").format(new Date());
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
         String imageFileName = "JPEG_" + timeStamp + "_";
         File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
         File image = File.createTempFile(imageFileName,".jpg", storageDir);
@@ -176,6 +214,7 @@ public class MainActivity extends AppCompatActivity {
         return image;
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.Q)
     private void dispatchTakePictureIntent() {
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
 
@@ -193,28 +232,116 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    @Override
+    protected void onActivityResult (int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_TAKE_PHOTO) {
+            if (resultCode == Activity.RESULT_OK) {
+
+                ExifInterface ei = null;
+                try {
+                    ei = new ExifInterface(currentPhotoPath);
+                } catch (IOException e) {
+                    //e.printStackTrace();
+                }
+                File f = new File(currentPhotoPath);
+
+                int orientation = ei.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL);
+                if (orientation == ExifInterface.ORIENTATION_ROTATE_90) {
+                    try {
+                        f = rotateImage(f, 90);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                } else if (orientation == ExifInterface.ORIENTATION_ROTATE_180) {
+                    try {
+                        f = rotateImage(f, 180);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                } else if (orientation == ExifInterface.ORIENTATION_ROTATE_270) {
+                    try {
+                        f = rotateImage(f, 270);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+                updateCaption(f);
+            }
+        }
+    }
+
+
+
     public void updatePhoto(String path, String caption) {
         String[] attr = path.split("_");
         if (attr.length > 4) {
-            File to = new File(attr[0] + "_" + caption + "_" + attr[2] + "_" + attr[3] + "_" + attr[4]);
+            File to = new File(attr[0] + "_" + attr[1] + "_" + attr[2] + "_" + caption +  "_" + attr[4]);
             File from = new File(path);
             from.renameTo(to);
         }else{
-            File to = new File(attr[0] + "_" + caption + "_" + attr[1] + "_" + attr[2] + "_" + attr[3]);
+            File to = new File(attr[0] + "_" + attr[1] + "_" + attr[2] + "_" + caption + "_" + attr[3]);
             File from = new File(path);
             from.renameTo(to);
         }
 
     }
 
-    public String updateCaption(String path) {
-        String[] attr = path.split("_");
-        String ret;
-        if (attr.length > 4) {
-            ret = attr[1];
+    public void updateCaption(File path) {
+        String path_str = path.getPath();
+        String date;
+
+        if (path_str == null || path_str =="") {
+            selectedImage.setImageResource(R.mipmap.ic_launcher);
+            date_time.setText("");
+            caption.setText("");
         } else {
-            ret = "noCaption";
+            String[] attr = path_str.split("_");
+            selectedImage.setImageBitmap(BitmapFactory.decodeFile(path_str));
+            if (attr.length > 4) {
+                caption.setText(attr[3]);
+                date = attr[1];
+            } else {
+                caption.setText("");
+                date = attr[1];
+            }
+            if (date.length() == 8) {
+                String date_format = date.substring(0,4) + "/" + date.substring(4,6) + "/" + date.substring(6,8);
+                date_time.setText(date_format);
+            } else {
+                date_time.setText(date);
+            }
         }
-        return ret;
     }
+
+    public void searchUpdate (File[] files, String[] param_list){
+
+        int index = 0;
+        int foundIndex = -1;
+        for (File f : files){
+            String path = f.getPath();
+            String[] attr = path.split("_");
+            if((param_list[0].length() == 0) && (param_list[1].length() == 0)) {
+                if (attr[3].equals(param_list[2])) {
+                    foundIndex = index;
+                    break;
+                }
+            }else if ((Integer.parseInt(attr[1]) >= Integer.parseInt(param_list[0]) && Integer.parseInt(attr[1]) <= Integer.parseInt(param_list[1]))
+                    && (param_list[2].equals("") || attr[3].equals(param_list[2]))) {
+                foundIndex = index;
+                break;
+            }
+            index++;
+        }
+
+        if(foundIndex == -1){
+            Toast.makeText(this, "No pictures found", Toast.LENGTH_SHORT).show();
+        }else {
+            updateCaption(files[foundIndex]);
+        }
+
+    }
+
+
+
 }
