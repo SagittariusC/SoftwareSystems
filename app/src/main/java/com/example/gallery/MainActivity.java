a8261509b112e9d31d9229539ff1d4f968e40995
SagittariusC <37080178+SagittariusC@users.noreply.github.com>
2021-02-05 01:06:01 -0800

    GeoTag Search partially implemented

--- app/src/main/java/com/example/gallery/MainActivity.java
+++ app/src/main/java/com/example/gallery/MainActivity.java
@@ -68,7 +68,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     public static final String MAPVIEW_BUNDLE_KEY = "MapViewBundleKey";
 
     ImageView selectedImage;
-    Button camera;
+    Button camera, filter;
     ImageButton left, right;
     FloatingActionButton share;
     String currentPhotoPath;
@@ -92,13 +92,14 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         share = findViewById(R.id.share_button);
         caption = findViewById(R.id.edit_caption);
         date_time = findViewById(R.id.timestamp);
+        filter = findViewById(R.id.filter_button);
 
         mMapView = findViewById(R.id.idLocationMap);
         Bundle mapViewBundle = null;
         if (savedInstanceState != null) {
             mapViewBundle = savedInstanceState.getBundle(MAPVIEW_BUNDLE_KEY);
         }
-        mMapView.onCreate(mapViewBundle);
+        mMapView.onCreate(savedInstanceState);
         mMapView.getMapAsync(this);
 
         ActivityCompat.requestPermissions(this, new String[]{
@@ -117,11 +118,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
             if (extras == null) {
                 searchParams = null;
             } else {
-                //searchParams[0] = extras.getString("STARTTIMESTAMP");
-                //searchParams[1] = extras.getString("ENDTIMESTAMP");
-                //searchParams[2] = extras.getString("CAPTION");
-                //searchUpdate(files, searchParams);
-                img_counter = extras.getInt("SearchedImgCounter");
+                img_counter = extras.getInt("SELECTEDSEARCHIMAGE");
                 updateCaption(files[img_counter]);
             }
         } else {
@@ -146,6 +143,14 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
             @Override
             public void onClick(View v) {
                 moveRight();
+            }
+        });
+
+        filter.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent i = new Intent(MainActivity.this, Filter.class);
+                startActivity(i);
             }
         });
 
@@ -202,7 +207,22 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
             outState.putBundle(MAPVIEW_BUNDLE_KEY, mapViewBundle);
         }
         mMapView.onSaveInstanceState(mapViewBundle);
+        super.onSaveInstanceState(outState);
     }
+
+    /*
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        Bundle mapViewBundle = outState.getBundle(MAPVIEW_BUNDLE_KEY);
+        if (mapViewBundle == null) {
+            mapViewBundle = new Bundle();
+            outState.putBundle(MAPVIEW_BUNDLE_KEY, mapViewBundle);
+        }
+        mMapView.onSaveInstanceState(mapViewBundle);
+    }
+    */
 
     @Override
     public void onResume() {
@@ -551,34 +571,6 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
                 date_time.setText(date);
             }
         }
-    }
-
-    public void searchUpdate (File[] files, String[] param_list){
-
-        int index = 0;
-        int foundIndex = -1;
-        for (File f : files){
-            String path = f.getPath();
-            String[] attr = path.split("_");
-            if((param_list[0].length() == 0) && (param_list[1].length() == 0)) {
-                if (attr[3].equals(param_list[2])) {
-                    foundIndex = index;
-                    break;
-                }
-            }else if ((Integer.parseInt(attr[1]) >= Integer.parseInt(param_list[0]) && Integer.parseInt(attr[1]) <= Integer.parseInt(param_list[1]))
-                    && (param_list[2].equals("") || attr[3].equals(param_list[2]))) {
-                foundIndex = index;
-                break;
-            }
-            index++;
-        }
-
-        if(foundIndex == -1){
-            Toast.makeText(this, "No pictures found", Toast.LENGTH_SHORT).show();
-        }else {
-            updateCaption(files[foundIndex]);
-        }
-
     }
 
 
