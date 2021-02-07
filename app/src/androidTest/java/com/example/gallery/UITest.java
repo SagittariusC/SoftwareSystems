15a327b247932443075f45b3e173140e90d36cfc
SagittariusC <37080178+SagittariusC@users.noreply.github.com>
2021-02-06 19:23:34 -0800

    Added LatLong display to main activity

--- app/src/androidTest/java/com/example/gallery/UITest.java
+++ app/src/androidTest/java/com/example/gallery/UITest.java
@@ -1,34 +1,20 @@
 package com.example.gallery;
 
-import android.app.Activity;
-import android.app.Instrumentation;
-import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.support.test.InstrumentationRegistry;
 import android.widget.DatePicker;
 
 import org.hamcrest.Matchers;
 import org.junit.Rule;
 import org.junit.Test;
 
-
-import androidx.test.espresso.Espresso;
-import androidx.test.espresso.action.ViewActions;
 import androidx.test.espresso.contrib.PickerActions;
-import androidx.test.espresso.intent.Intents;
-import androidx.test.espresso.intent.matcher.IntentMatchers;
 import androidx.test.espresso.intent.rule.IntentsTestRule;
-import androidx.test.espresso.matcher.ViewMatchers;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static androidx.test.espresso.action.ViewActions.typeText;
-import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.withClassName;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
-import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
 
 public class UITest {
@@ -60,8 +46,8 @@ public class UITest {
     }*/
 
     @Test
-    public void searchCaption() throws InterruptedException {
-        Thread.sleep(1000);
+    public void searchAndShare() throws InterruptedException {
+        Thread.sleep(3000);
         onView(withId(R.id.filter_button)).perform(click());
         Thread.sleep(1000);
 
@@ -70,18 +56,18 @@ public class UITest {
         onView(withId(android.R.id.button1)).perform(click());
         Thread.sleep(1000);
         onView(withId(R.id.endDateText)).perform(click());
-        onView(withClassName(Matchers.equalTo(DatePicker.class.getName()))).perform(PickerActions.setDate(2021, 2, 7));
+        onView(withClassName(Matchers.equalTo(DatePicker.class.getName()))).perform(PickerActions.setDate(2021, 2, 6));
         onView(withId(android.R.id.button1)).perform(click());
         Thread.sleep(1000);
-        onView(withId(R.id.captionText)).perform(typeText("do"), closeSoftKeyboard());
+        onView(withId(R.id.captionText)).perform(typeText("test"), closeSoftKeyboard());
         Thread.sleep(1000);
-        onView(withId(R.id.TopLeftLat)).perform(typeText("48.86"), closeSoftKeyboard());
+        onView(withId(R.id.TopLeftLat)).perform(typeText("54"), closeSoftKeyboard());
         Thread.sleep(1000);
-        onView(withId(R.id.TopLeftLong)).perform(typeText("2.25"), closeSoftKeyboard());
+        onView(withId(R.id.TopLeftLong)).perform(typeText("-125"), closeSoftKeyboard());
         Thread.sleep(1000);
-        onView(withId(R.id.BottomRightLat)).perform(typeText("48.84"), closeSoftKeyboard());
+        onView(withId(R.id.BottomRightLat)).perform(typeText("46"), closeSoftKeyboard());
         Thread.sleep(1000);
-        onView(withId(R.id.BottomRightLong)).perform(typeText("2.35"), closeSoftKeyboard());
+        onView(withId(R.id.BottomRightLong)).perform(typeText("-110"), closeSoftKeyboard());
         Thread.sleep(1000);
         onView(withId(R.id.searchButton)).perform(click());
         Thread.sleep(4000);
@@ -92,8 +78,9 @@ public class UITest {
         onView(withId(R.id.button_left)).perform(click());
         Thread.sleep(2000);
         onView(withId(R.id.searchImageView)).perform(click());
-        Thread.sleep(5000);
-
+        Thread.sleep(3000);
+        onView(withId(R.id.share_button)).perform(click());
+        Thread.sleep(10000);
         
     }
 }
