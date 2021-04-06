778995a16149c392e3771b5f09e598c88c2897ca
Max Horie <maxhorie@live.ca>
2021-04-06 12:26:07 -0700

    Added ability to change password

--- app/src/main/java/com/example/gallery/Password.java
+++ app/src/main/java/com/example/gallery/Password.java
@@ -1,12 +1,10 @@
 package com.example.gallery;
 
-import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Color;
-import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.text.Editable;
@@ -22,26 +20,22 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.io.UnsupportedEncodingException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
-import java.util.Scanner;
 import java.util.regex.Pattern;
 
 import javax.crypto.Cipher;
-import javax.crypto.SecretKey;
-import javax.crypto.SecretKeyFactory;
-import javax.crypto.spec.DESKeySpec;
 import javax.crypto.spec.SecretKeySpec;
 
 
@@ -50,10 +44,12 @@ public class Password extends AppCompatActivity {
     File[] documents;
     String path;
     boolean pw = false;
+    boolean settings = false;
     Button submit;
     EditText pass;
     TextView message;
     TextView up, low, number, char_len;
+    FloatingActionButton change_pw;
     String password;
     int count = 5;
     boolean strength;
@@ -69,6 +65,7 @@ public class Password extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_password);
 
+        change_pw = findViewById(R.id.settings);
         submit =findViewById(R.id.checkpw);
         pass = findViewById(R.id.password_field);
         password = pass.getText().toString();
@@ -96,10 +93,7 @@ public class Password extends AppCompatActivity {
         key = Arrays.copyOf(key, 16); // use only first 128 bit
 
         sks = new SecretKeySpec(key, "AES");
-//
-//        for (File f : documents) {
-//            if (f.toString() == path) pw = true;
-//        }
+
         if (new File(path).isFile()) pw = true;
 
         if (!pw) {
@@ -108,6 +102,8 @@ public class Password extends AppCompatActivity {
             number.setText("");
             char_len.setText("");
             message.setText("No password has been set for this gallery");
+            change_pw.setAlpha(0.0f);
+
             pass.addTextChangedListener(new TextWatcher() {
                 @Override
                 public void beforeTextChanged(CharSequence s, int start, int count, int after) {
@@ -136,6 +132,17 @@ public class Password extends AppCompatActivity {
             number.setText("");
             char_len.setText("");
             message.setText("The gallery is locked");
+
+            change_pw.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    try {
+                        changePassword();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
         }
 
         pass.setOnEditorActionListener(new TextView.OnEditorActionListener() {
@@ -155,12 +162,35 @@ public class Password extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 try {
-                    checkPassword();
+                    if (!settings) {
+                        Intent intent = new Intent(Password.this, MainActivity.class);
+                        if (checkPassword()) {
+                            startActivity(intent);
+                        }
+                    } else {
+                        if (checkPassword() && pw) {
+                            message.setText("Correct! Choose a new password");
+                            pw = false;
+                            File f = new File(path);
+                            f.delete();
+                            Intent intent = new Intent(Password.this, Password.class);
+                            startActivity(intent);
+                        } else if (checkPassword() && !pw) {
+                            Intent intent = new Intent(Password.this, MainActivity.class);
+                            startActivity(intent);
+                        }
+                    }
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
             }
         });
+    }
+
+    private void changePassword() throws IOException {
+        settings = true;
+        message.setText("Enter the old password for this gallery");
+        submit.setText("Change Password");
     }
 
     public boolean validate(String password) {
@@ -174,26 +204,29 @@ public class Password extends AppCompatActivity {
         } else {
             low.setTextColor(Color.GREEN);
             count++;
-
         }
+
         if (!uppercase.matcher(password).find()) {
             up.setTextColor(Color.GRAY);
         } else {
             up.setTextColor(Color.GREEN);
             count++;
         }
+
         if (!numeric.matcher(password).find()) {
             number.setTextColor(Color.GRAY);
         } else {
             number.setTextColor(Color.GREEN);
             count++;
         }
+
         if (password.length() < 8) {
             char_len.setTextColor(Color.RED);
         } else if (password.length() > 7) {
             char_len.setTextColor(Color.GREEN);
             count++;
         }
+
         if (count == 4) {
             return true;
         } else {
@@ -201,7 +234,7 @@ public class Password extends AppCompatActivity {
         }
     }
 
-    private void checkPassword() throws IOException {
+    private boolean checkPassword() throws IOException {
 
         File stored_password = new File(path);
         password = pass.getText().toString();
@@ -209,62 +242,78 @@ public class Password extends AppCompatActivity {
             try {
                 if (strength) {
                     encodedBytes = null;
+
                     try {
                         Cipher c = Cipher.getInstance("AES");
                         c.init(Cipher.ENCRYPT_MODE, sks);
                         encodedBytes = c.doFinal(password.getBytes());
+
                     } catch (Exception e) {
                         Log.e(TAG, "AES encryption error");
                     }
+
                     FileOutputStream fOut = new FileOutputStream(stored_password);
                     OutputStreamWriter myOutWriter = new OutputStreamWriter(fOut);
                     myOutWriter.append(Base64.encodeToString(encodedBytes, Base64.DEFAULT));
                     myOutWriter.close();
                     fOut.close();
                     Toast.makeText(this, "Password set!", Toast.LENGTH_SHORT).show();
-                    Intent intent = new Intent(this, MainActivity.class);
-                    startActivity(intent);
+//                    Intent intent = new Intent(this, MainActivity.class);
+//                    startActivity(intent);
+                    return true;
+
                 } else {
                     Toast.makeText(this, "Password isn't strong enough!", Toast.LENGTH_SHORT).show();
                 }
+
             } catch (IOException e) {
                 e.printStackTrace();
             }
+
         } else {
             try {
-//                Scanner read_pw = new Scanner(stored_password);
-//                String stored = read_pw.nextLine();
                 BufferedReader read_pw = new BufferedReader(new FileReader(stored_password));
                 String stored = read_pw.readLine();
                 decodedBytes = null;
                 encodedBytes = stored.getBytes();
+
                 try {
                     Cipher c = Cipher.getInstance("AES");
                     c.init(Cipher.DECRYPT_MODE, sks);
                     decodedBytes = Base64.decode(stored.getBytes(), Base64.DEFAULT);
                     decodedBytes = c.doFinal(decodedBytes);
+
                 } catch (Exception e) {
                     Log.e(TAG, "AES decryption error");
                 }
+
                 String decoded = new String(decodedBytes,"UTF-8");
+
                 if (decoded.equals(password)) {
                     Toast.makeText(this, "Password correct!", Toast.LENGTH_SHORT).show();
-                    Intent intent = new Intent(this, MainActivity.class);
-                    startActivity(intent);
+//                    Intent intent = new Intent(this, MainActivity.class);
+//                    startActivity(intent);
+                    return true;
+
                 } else {
                     if (--count == 0) {
                         throw new RuntimeException("you idiot");
                     }
+
                     if (count == 1) {
                         Toast.makeText(this, "Wrong password! 1 try left...", Toast.LENGTH_SHORT).show();
+
                     } else {
                         Toast.makeText(this, "Wrong password! " + count + " tries left...", Toast.LENGTH_SHORT).show();
                     }
+
                     pass.getText().clear();
                 }
+
             } catch (FileNotFoundException e) {
                 e.printStackTrace();
             }
         }
+        return false;
     }
 }
