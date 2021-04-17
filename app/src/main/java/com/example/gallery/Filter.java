6278097934740e667e429ac69be3670a8a5d6f5b
shaunt123 <shauntyers@gmail.com>
2021-04-16 18:03:54 -0700

    Added city radius search functionality

--- app/src/main/java/com/example/gallery/Filter.java
+++ app/src/main/java/com/example/gallery/Filter.java
@@ -5,6 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.os.Handler;
+import android.os.Message;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
@@ -14,13 +17,17 @@ import android.widget.Toast;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Locale;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Filter extends AppCompatActivity {
 
     final Calendar myCalendar = Calendar.getInstance();
     Button searchButton, cancelButton;
-    EditText startDateText, endDateText;
+    EditText startDateText, endDateText, cityName;
     Boolean startDateEdit = false, endDateEdit = false;
+    Float latitudeFromCity;
+    Float longitudeFromCity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +38,7 @@ public class Filter extends AppCompatActivity {
         cancelButton = findViewById(R.id.cancelButton);
         startDateText= (EditText) findViewById(R.id.startDateText);
         endDateText= (EditText) findViewById(R.id.endDateText);
+        cityName = findViewById(R.id.cityText);
 
         cancelButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -43,7 +51,20 @@ public class Filter extends AppCompatActivity {
         searchButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick (View v) {
-                go(v);
+                if(!cityName.getText().toString().isEmpty()) {
+                    String address = cityName.getText().toString();
+                    GeoLocation geoLocation = new GeoLocation();
+                    geoLocation.getAddress(address, getApplicationContext(), new GeoHandler());
+                }
+
+               Handler handler = new Handler();
+               handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                    go(v);
+                    }
+                }, 500);
+
             }
         });
 
@@ -118,7 +139,14 @@ public class Filter extends AppCompatActivity {
         EditText TopLeftLong = (EditText) findViewById(R.id.TopLeftLong);
         EditText BottomRightLat = (EditText) findViewById(R.id.BottomRightLat);
         EditText BottomRightLong = (EditText) findViewById(R.id.BottomRightLong);
-
+        EditText radiusFromCity = (EditText) findViewById(R.id.radiusNumber);
+        if(!cityName.getText().toString().isEmpty()) {
+            float radiusInDeg = Float.parseFloat(radiusFromCity.getText().toString()) / 111;
+            TopLeftLat.setText(String.valueOf(latitudeFromCity + radiusInDeg));
+            BottomRightLat.setText(String.valueOf(latitudeFromCity - radiusInDeg));
+            TopLeftLong.setText(String.valueOf(longitudeFromCity - radiusInDeg));
+            BottomRightLong.setText(String.valueOf(longitudeFromCity + radiusInDeg));
+        }
         if( fromDate.length() == 0 && toDate.length() == 0 && caption.length() == 0 && TopLeftLat.length() == 0 && TopLeftLong.length() == 0 && BottomRightLat.length() == 0 && BottomRightLong.length() == 0 ){
             Toast.makeText(this, "No filter criteria entered", Toast.LENGTH_SHORT).show();
             Intent intent = new Intent(this, MainActivity.class);
@@ -131,9 +159,30 @@ public class Filter extends AppCompatActivity {
             i.putExtra("TOPLEFTLONG", TopLeftLong.length() != 0 ? Float.parseFloat(TopLeftLong.getText().toString()) : -1000);
             i.putExtra("BOTTOMRIGHTLAT", BottomRightLat.length() != 0 ? Float.parseFloat(BottomRightLat.getText().toString()) : -1000);
             i.putExtra("BOTTOMRIGHTLONG", BottomRightLong.length() != 0 ? Float.parseFloat(BottomRightLong.getText().toString()) : 1000);
-
             startActivity(i);
         }
     }
+    private class GeoHandler extends Handler {
+        @Override
+        public void handleMessage(Message msg){
+            String address;
+
+            switch (msg.what) {
+                case 1:
+                    Bundle bundle = msg.getData();
+                    address = bundle.getString("address");
+                    break;
+                default:
+                    address = null;
+            }
+            Pattern pattern = Pattern.compile("\n *");
+            Matcher matcher = pattern.matcher(address);
+            if (matcher.find()) {
+                latitudeFromCity = Float.parseFloat(address.substring(0, matcher.start()));
+                longitudeFromCity = Float.parseFloat(address.substring(matcher.end()));
+            }
+        }
+    }
 }
+
 
